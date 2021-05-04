7ee754b9fff629202fdaaed698675187370830cf
unknown <huytung139@gmail.com>
2021-05-04 21:08:01 +0700

    update

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import { View, Button, StyleSheet, Text } from 'react-native'
-import  TicTacToeScreen from './gameScreen/tictac'
-import TabScreen from './gameScreen/TabScreen'
-import DinoScreen from './gameScreen/DinoScreen'
-import MineSweepScreen from './gameScreen/MineSweepScreen'
-import BallScreen from './gameScreen/BallScreen'
-import SnakeScreen from './gameScreen/SnakeScreen' 
 import Main from './Navigator/Main'
 
 export default function App() {
@@ -13,13 +6,4 @@ export default function App() {
     <Main />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: "#bdbdbd",
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
 
