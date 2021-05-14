b55dd0ecee2b272a7f8eaccf76b30facc9bd961f
augustustung <huytung139@gmail.com>
2021-05-14 13:44:58 +0700

    add ping pong game

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import PingPongScreen from './gameScreen/PingPongScreen'
 import Main from './Navigator/Main'
+
 
 export default function App() {
   return (
-    <Main />
+    <PingPongScreen />
   )
 }
 
